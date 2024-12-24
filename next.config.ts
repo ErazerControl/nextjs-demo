bbb361e4b520cd9dffe99a45b6c066e885d28c5b
ErazerControl <erazer_control@163.com>
2024-12-24 22:09:44 +0800

    add

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    ppr: "incremental",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
